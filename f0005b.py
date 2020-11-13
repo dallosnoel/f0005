0b58209c55c8f91bf7c38d5e9bd08c2f64c5c245
dallosnoel <dallosnoel@gmail.com>
2020-11-13 08:39:28 +0000

    Initial commit

---
+++ f0005b.py
@@ -0,0 +1,7 @@
+jelszó = 'random'
+tipp = input('Mi a jelszó?: ')
+
+if tipp == jelszó:
+    print('Bemehet.')
+else:
+    print('Hozzáférés megtagadva.')
---
+++ f0005c.py
@@ -0,0 +1,9 @@
+szam1 = 5
+szam2 = 18
+
+eredmény = szam1 + szam2
+tipp = int(input('Írd be a megoldást '))
+if eredmény == tipp:
+  print('ügyes vagy')
+else:
+  print('béna vagy')
